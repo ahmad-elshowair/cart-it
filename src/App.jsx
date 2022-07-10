1e99018f73f65e2bf69a65c0a61176c00027a4eb
Ahmad Elshowair <it.is.work@outlook.com>
2022-07-10 22:46:08 +0200

    add bootstrap package

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <main className="app">
-      
+      <h1>Cart it</h1>
     </main>
   );
 }
 
 export default App;
+  
