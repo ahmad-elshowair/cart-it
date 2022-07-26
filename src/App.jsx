96b9821ac12f9daff1f62bc53d430199ea7da593
Ahmad Elshowair <it.is.work@outlook.com>
2022-07-26 17:42:49 +0200

    add Home and Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-      <main className="app">
         <Header/>
-      </main>
     </BrowserRouter>
   );
 }
