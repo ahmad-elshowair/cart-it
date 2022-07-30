3359bfd55eb0c2f00b213a8397310727caa795aa
Ahmad Elshowair <it.is.work@outlook.com>
2022-07-30 09:09:44 +0200

    Updates

---
+++ precache-manifest.4fe434cf7d2f41b8d9b33f2e8aa51e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bd75d46b3d66476036e",
+    "url": "/cart-it/static/css/main.278c88c2.chunk.css"
+  },
+  {
+    "revision": "5bd75d46b3d66476036e",
+    "url": "/cart-it/static/js/main.5bd75d46.chunk.js"
+  },
+  {
+    "revision": "c0a07d35faf03c50729b",
+    "url": "/cart-it/static/css/1.920693fe.chunk.css"
+  },
+  {
+    "revision": "c0a07d35faf03c50729b",
+    "url": "/cart-it/static/js/1.c0a07d35.chunk.js"
+  },
+  {
+    "revision": "50b50bfebc3e09e0f88e",
+    "url": "/cart-it/static/js/runtime~main.50b50bfe.js"
+  },
+  {
+    "revision": "026cb24e5fdea00efa706a009900f8db",
+    "url": "/cart-it/index.html"
+  }
+];
